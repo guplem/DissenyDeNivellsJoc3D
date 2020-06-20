bf986be2b1be33540a47703838c2aed635f3e2b1
Guillem Poy <guplem@gmail.com>
2020-06-20 22:02:35 +0200

    Fixed major bug?

--- DissenyDeNivellsJoc3D/Assets/CameraDetector.cs
+++ DissenyDeNivellsJoc3D/Assets/CameraDetector.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CameraDetector : MonoBehaviour
 {
+    [SerializeField] private Transform cameraPosition;
     private void OnTriggerStay(Collider other)
     {
         CharacterController controller = other.gameObject.GetComponent<CharacterController>();
         if (controller != null)
         {
             RaycastHit hit;
-            if (!Physics.Linecast(transform.position, controller.transform.position+Vector3.up, out hit)) {
+            if (!Physics.Linecast(cameraPosition.position, controller.transform.position+Vector3.up, out hit)) {
                 // Nothing in between
                 Debug.Log($"Player seen by {transform.parent.parent.gameObject.name}!", gameObject);
+                Debug.DrawLine(cameraPosition.position, controller.transform.position+Vector3.up*0.25f, Color.yellow, 10);
+                Debug.Break();
                 controller.enabled = false;
                 controller.transform.SetProperties(GameManager.Instance.transform);
                 controller.enabled = true;
