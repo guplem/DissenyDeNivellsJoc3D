5d7b20ae97e075ee8b720cb7a5ca2619407beedc
Guillem Poy <guplem@gmail.com>
2020-06-21 01:32:47 +0200

    Improvements

--- DissenyDeNivellsJoc3D/Assets/CameraDetector.cs
+++ DissenyDeNivellsJoc3D/Assets/CameraDetector.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class CameraDetector : MonoBehaviour
 {
     [SerializeField] private Transform cameraPosition;
+
+    private Vector3 heightCheck = Vector3.up * 0.1f;
     private void OnTriggerStay(Collider other)
     {
         CharacterController controller = other.gameObject.GetComponent<CharacterController>();
         if (controller != null)
         {
             RaycastHit hit;
-            if (!Physics.Linecast(cameraPosition.position, controller.transform.position+Vector3.up, out hit)) {
+            if (!Physics.Linecast(cameraPosition.position, controller.transform.position+heightCheck, out hit)) {
                 // Nothing in between
                 Debug.Log($"Player seen by {transform.parent.parent.gameObject.name}!", gameObject);
-                Debug.DrawLine(cameraPosition.position, controller.transform.position+Vector3.up*0.25f, Color.yellow, 10);
+                Debug.DrawLine(cameraPosition.position, controller.transform.position+heightCheck, Color.yellow, 10);
                 Debug.Break();
                 controller.enabled = false;
                 controller.transform.SetProperties(GameManager.Instance.transform);
