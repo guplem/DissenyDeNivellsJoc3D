5331483da2dcacf763d4734480684586e174a9ce
Guillem Poy <guplem@gmail.com>
2020-06-20 18:50:12 +0200

    All doors working

---
+++ DissenyDeNivellsJoc3D/Assets/LockedDoor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockedDoor : SimpleAnimationInteractable
+{
+    public enum Locking
+    {
+        Key,
+        Id,
+        Diamond,
+    }
+
+    public Locking itemNeeded;
+    
+    public override void Interact()
+    {
+        switch (itemNeeded)
+        {
+            case Locking.Key:
+                if (!GameManager.Instance.hasKey) return;
+                break;
+            case Locking.Id:
+                if (!GameManager.Instance.hasId) return;
+                break;
+            case Locking.Diamond:
+                if (!GameManager.Instance.hasDiamond) return;
+                break;
+        }
+
+        base.Interact();
+    }
+}
